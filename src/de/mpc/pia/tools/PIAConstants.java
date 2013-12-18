053c9900e3eae6a81b3435aededbb5aa44931135
julianu <julian.uszkoreit@rub.de>
2013-12-18 13:01:23 +0100

    PIA related entries of the PSI-MS OBO as constants

--- src/de/mpc/pia/tools/PIAConstants.java
+++ src/de/mpc/pia/tools/PIAConstants.java
@@ -70,14 +70,75 @@ public class PIAConstants {
 	public static final String CV_PSI_MS_LABEL = "MS";
 	
 	/** PIAs accession in the PSI-CV*/
-	public static final String CV_PIA_ACCESSION = "MS:XXXXX";
+	public static final String CV_PIA_ACCESSION = "MS:1002387";
 	/** PIAs name in the PSI-CV*/
 	public static final String CV_PIA_NAME = "PIA";
 	
+	/** accession for the "PIA XML file" in PSI-MS */
+	public static final String CV_PIA_XML_FILE_ACCESSION = "MS:1002388";
+	/** name for the "PIA XML file" in PSI-MS */
+	public static final String CV_PIA_XML_FILE_NAME = "PIA XML file";
+	
+	/** accession for the "PIA workflow parameter" in PSI-MS */
+	public static final String CV_PIA_WORKFLOW_PARAMETER_ACCESSION = "MS:1002389";
+	/** name for the "PIA workflow parameter" in PSI-MS */
+	public static final String CV_PIA_WORKFLOW_PARAMETER_NAME = "PIA workflow parameter";
+	
+	/** accession for the "PIA:FDRScore calculated" in PSI-MS */
+	public static final String CV_PIA_FDRSCORE_CALCULATED_ACCESSION = "MS:1002390";
+	/** name for the "PIA:FDRScore calculated" in PSI-MS */
+	public static final String CV_PIA_FDRSCORE_CALCULATEDNAME = "PIA:FDRScore calculated";
+	
+	/** accession for the "PIA:Combined FDRScore calculated" in PSI-MS */
+	public static final String CV_PIA_COMBINED_FDRSCORE_CALCULATED_ACCESSION = "MS:1002391";
+	/** name for the "PIA:Combined FDRScore calculated" in PSI-MS */
+	public static final String CV_PIA_COMBINED_FDRSCORE_CALCULATED_NAME = "PIA:Combined FDRScore calculated";
+	
+	/** accession for the "PIA:PSM sets created" in PSI-MS */
+	public static final String CV_PIA_PSM_SETS_CREATED_ACCESSION = "MS:1002392";
+	/** name for the "PIA:PSM sets created" in PSI-MS */
+	public static final String CV_PIA_PSM_SETS_CREATED_DNAME = "PIA:PSM sets created";
+	
+	/** accession for the "PIA:used top identifications for FDR" in PSI-MS */
+	public static final String CV_PIA_USED_TOP_IDENTIFICATIONS_ACCESSION = "MS:1002393";
+	/** name for the "PIA:used top identifications for FDR" in PSI-MS */
+	public static final String CV_PIA_USED_TOP_IDENTIFICATIONS_DNAME = "PIA:used top identifications for FDR";
+	
 	/** accession for the "PIA protein score" in PSI-MS */
-	public static final String CV_PIA_PROTEIN_SCORE_ACCESSION = "MS:XXXXX";
+	public static final String CV_PIA_PROTEIN_SCORE_ACCESSION = "MS:1002394";
 	/** name for the "PIA protein score" in PSI-MS */
-	public static final String CV_PIA_PROTEIN_SCORE_NAME = "PIA protein score";
+	public static final String CV_PIA_PROTEIN_SCORE_NAME = "PIA:protein score";
+	
+	/** accession for the "PIA:protein inference" in PSI-MS */
+	public static final String CV_PIA_PROTEIN_INFERENCE_ACCESSION = "MS:1002395";
+	/** name for the "PIA:protein inference" in PSI-MS */
+	public static final String CV_PIA_PROTEIN_INFERENCE_NAME = "PIA:protein inference";
+	
+	/** accession for the "PIA:protein inference filter" in PSI-MS */
+	public static final String CV_PIA_PROTEIN_INFERENCE_FILTER_ACCESSION = "MS:1002396";
+	/** name for the "PIA:protein inference filter" in PSI-MS */
+	public static final String CV_PIA_PROTEIN_INFERENCE_FILTER_NAME = "PIA:protein inference filter";
+	
+	/** accession for the "PIA:protein inference scoring" in PSI-MS */
+	public static final String CV_PIA_PROTEIN_INFERENCE_SCORING_ACCESSION = "MS:1002397";
+	/** name for the "PIA:protein inference scoring" in PSI-MS */
+	public static final String CV_PIA_PROTEIN_INFERENCE_SCORING_NAME = "PIA:protein inference scoring";
+	
+	/** accession for the "PIA:protein inference used score" in PSI-MS */
+	public static final String CV_PIA_PROTEIN_INFERENCE_USED_SCORE_ACCESSION = "MS:1002398";
+	/** name for the "PIA:protein inference used score" in PSI-MS */
+	public static final String CV_PIA_PROTEIN_INFERENCE_USED_SCORE_NAME = "PIA:protein inference used score";
+	
+	/** accession for the "PIA:protein inference used PSMs" in PSI-MS */
+	public static final String CV_PIA_PROTEIN_INFERENCE_USED_PSMS_ACCESSION = "MS:1002399";
+	/** name for the "PIA:protein inference used PSMs" in PSI-MS */
+	public static final String CV_PIA_PROTEIN_INFERENCE_USED_PSMS_NAME = "PIA:protein inference used PSMs";
+	
+	/** accession for the "PIA:protein inference used PSMs" in PSI-MS */
+	public static final String CV_PIA_FILTER_ACCESSION = "MS:1002400";
+	/** name for the "PIA:protein inference used PSMs" in PSI-MS */
+	public static final String CV_PIA_FILTER_NAME = "PIA:filter";
+	
 	
 	/** accession for "PSM-level FDRScore" in PSI-MS */
 	public static final String CV_PSM_LEVEL_FDRSCORE_ACCESSION = "MS:1002355";
