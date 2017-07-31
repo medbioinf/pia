10042af8c5e4824bc44bafa349da225c5e6658d7
Yasset Perez-Riverol <ypriverol@gmail.com>
2017-07-31 13:22:31 +0100

    test FDR at peptide level updated

--- src/test/java/de/mpc/pia/modeller/PeptideMzTabModellerTest.java
+++ src/test/java/de/mpc/pia/modeller/PeptideMzTabModellerTest.java
@@ -4,8 +4,10 @@ import de.mpc.pia.intermediate.compiler.PIACompiler;
 import de.mpc.pia.intermediate.compiler.PIASimpleCompiler;
 import de.mpc.pia.intermediate.compiler.parser.InputFileParserFactory;
 import de.mpc.pia.modeller.exporter.MzTabExporter;
+import de.mpc.pia.modeller.peptide.ReportPeptide;
 import de.mpc.pia.modeller.report.filter.FilterComparator;
 import de.mpc.pia.modeller.report.filter.impl.PSMScoreFilter;
+import de.mpc.pia.modeller.report.filter.impl.PeptideScoreFilter;
 import de.mpc.pia.modeller.score.FDRData;
 import de.mpc.pia.modeller.score.ScoreModelEnum;
 import de.mpc.pia.modeller.score.FDRData.DecoyStrategy;
@@ -17,6 +19,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -75,15 +78,23 @@ public class PeptideMzTabModellerTest {
         // calculate peptide FDR and filter
         piaModeller.getPeptideModeller().addFilter(0L, new PSMScoreFilter(FilterComparator.less_equal, false, 0.01, ScoreModelEnum.PSM_LEVEL_FDR_SCORE.getShortName()));
 
-        fileID = 1L;
+        fileID = 0L;
         piaModeller.getPeptideModeller().calculateFDR(fileID);
 
         fdrData = piaModeller.getPeptideModeller().getFilesFDRData(fileID);
 
-        assertEquals("psm_fdr_score", fdrData.getScoreShortName());
+        assertEquals("psm_combined_fdr_score", fdrData.getScoreShortName());
         assertEquals(2, fdrData.getNrDecoys().intValue());
         assertEquals(1260, fdrData.getNrItems().intValue());
         assertEquals(1258, fdrData.getNrTargets().intValue());
+
+        piaModeller.getPeptideModeller().addFilter(0L,
+                new PeptideScoreFilter(FilterComparator.less_equal, false, 0.01, ScoreModelEnum.PEPTIDE_LEVEL_Q_VALUE.getShortName())
+        );
+
+        List<ReportPeptide> peptides = piaModeller.getPeptideModeller().getFilteredReportPeptides(0L, piaModeller.getPeptideModeller().getFilters(0L));
+
+        peptides.forEach(peptide -> System.out.println(peptide.getSequence() + " -> " + peptide.getQValue()));
 
         MzTabExporter exporter = new MzTabExporter(piaModeller);
         File exportFile = File.createTempFile("control_exo_rep1_high_mol_weight", "mzTab");
